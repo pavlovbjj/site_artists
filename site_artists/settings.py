fb8b1f10f301e347fdb6455014895d3079ee203f
Dmitry Pavlov <96339342+pavlovbjj@users.noreply.github.com>
2022-12-02 13:18:58 +0500

    Update settings.py

--- site_artists/settings.py
+++ site_artists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#dh8s^==0jinxcx)_*$#&wpdrgj-dt81(d!on8=3nzdl0qnx^v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
